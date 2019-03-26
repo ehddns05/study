2382968cf2d27314c772bf6892df6e7c58e25ab3
dongdong <dongwun05@gmail.com>
2019-03-26 23:53:51 +0900

    update 1

---
+++ Struts_Sample/src/action/ListAction.java
@@ -0,0 +1,13 @@
+package action;
+
+import com.opensymphony.xwork2.Action;
+
+public class ListAction implements Action{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		
+		return SUCCESS;
+	}
+}
---
+++ Struts_Sample/src/ibatis/IbatisDAO.java
@@ -0,0 +1,5 @@
+package ibatis;
+
+public class IbatisDAO {
+	
+}
